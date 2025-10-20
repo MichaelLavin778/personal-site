69a50a3cd6d01d070cc4eb9a855c8b853bd2d45a
Michael Lavin <43680530+MichaelLavin778@users.noreply.github.com>
2025-10-20 14:49:50 -0600

    Footer + eslint shell

---
+++ src/footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FooterContainer, FooterRow, FooterColumn } from './FooterStyles';
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterRow>
+                <FooterColumn>
+                    <h3>Follow Us</h3>
+                    <p>Social media links and icons.</p>
+                </FooterColumn>
+            </FooterRow>
+        </FooterContainer>
+    );
+};
+
+export default Footer;
