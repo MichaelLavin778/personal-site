f7d91fefa741dda6bbb1000f7b32bf2ac377505a
Michael Lavin <43680530+MichaelLavin778@users.noreply.github.com>
2025-10-20 17:15:43 -0600

    social

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { FooterContainer, FooterRow, FooterColumn } from './FooterStyles';
+import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => {
     return (
         <FooterContainer>
             <FooterRow>
                 <FooterColumn>
-                    <h3>Follow Us</h3>
-                    <p>Social media links and icons.</p>
+                    <SocialIcon url="https://linkedin.com/in/michael-lavin-2373b7198/" />
                 </FooterColumn>
             </FooterRow>
         </FooterContainer>
